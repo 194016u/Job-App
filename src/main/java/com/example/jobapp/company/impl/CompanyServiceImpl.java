e3b0fda67d0b54b2a97b6fe38227ba9256932d86
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-04-22 13:20:04 +0530

    create get http request

--- src/main/java/com/example/jobapp/company/impl/CompanyServiceImpl.java
+++ src/main/java/com/example/jobapp/company/impl/CompanyServiceImpl.java
@@ -55,4 +55,9 @@ public class CompanyServiceImpl implements CompanyService {
         }
 
     }
+
+    @Override
+    public Company getCompanyById(Long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
 }
